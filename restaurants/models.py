cfb8cccf76091e50223e5875c13e3f337a75a395
Ubuntu <ubuntu@ip-172-31-44-130.us-east-2.compute.internal>
2019-04-17 20:38:12 +0000

    update restaurants

--- restaurants/models.py
+++ restaurants/models.py
@@ -172,10 +172,10 @@ def registerNon(email, password, address, name, phoneNumber, zip_code):
     # pprint.pprint(row)
     return [email, password, name, address, phoneNumber, zip_code]
 
-def registerRes(email, password, address, name, phoneNumber, zip_code):
+def registerRes(email, password, address, name, phoneNumber, zip_code, city):
     cursor = connection.cursor()
     rating = str(0);
-    cursor.execute('INSERT INTO restaurants ("email", "password", "name", "address", "phone_number", "zip_code", "rating") VALUES(%s, %s, %s, %s, %s, %s, %s) ', [email, password, name, address, phoneNumber, zip_code, rating])
+    cursor.execute('INSERT INTO restaurants ("email", "password", "name", "address", "phone_number", "zip_code", "rating", "city") VALUES(%s, %s, %s, %s, %s, %s, %s, %s) ', [email, password, name, address, phoneNumber, zip_code, rating, city])
     # row = cursor.fetchall()
     # pprint.pprint(row)
     return [email, password, name, address, phoneNumber, zip_code, rating]
